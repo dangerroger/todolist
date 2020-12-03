745d570c4e6251786af3c6c52ddaf3435f7dad36
iamrahul <drseewal@gmail.com>
2020-12-03 14:58:16 +0000

    Model for list items and associated migrations

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+class Item(models.Model):
+    """docstring for Item."""
+    text = models.TextField(default = '')
+
+
+    #def __init__(self, arg):
+        #super(Item, self).__init__()
+        #self.arg = arg
 
 # Create your models here.
