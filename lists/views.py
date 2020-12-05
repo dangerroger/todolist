4ea0eedd1e68abd03df16968c4dfdf5141a54429
iamrahul <drseewal@gmail.com>
2020-12-05 07:49:50 +0000

    please enter the commit message for your changes. Lines starting git commit -am"model for list items"

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    #if request.method == 'POST':
+    if request.method == 'POST':
+        #new_item_text = request.POST['item_text']
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    #else:
+        #new_item_text = ''
+
         #return HttpResponse(request.POST['item_text'])
-        return render(request,'home.html', {
-            'new_item_text': request.POST.get('item_text', ''),
-            })
+        #item = Item()
+        #item.text = request.POST.get('item_text', '')
+        #item.save()
+
+    return render(request,'home.html')
+
+    #, {
+            #'new_item_text': new_item_text
+            #}
