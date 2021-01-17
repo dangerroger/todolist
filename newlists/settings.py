7f55ba972a96a8c35e0300fd450f0aa7ea620e57
iamrahul <drseewal@gmail.com>
2021-01-17 10:33:44 +0000

    move sqlite database outside of main source tree

--- newlists/settings.py
+++ newlists/settings.py
@@ -77,7 +77,9 @@ WSGI_APPLICATION = 'newlists.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # changed after nginx deployment
+        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
     }
 }
 
